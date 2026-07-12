3885076bab83883fa6749d6d3a8afb0da0e3e3a2
worldsolidaryart-cmyk <worldsolidaryart@gmail.com>
2026-07-12 07:47:59 -0300

    Add Google Translate component

---
+++ src/components/GoogleTranslate.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+
+declare global {
+  interface window {
+    googleTranslateElementInit: () => void;
+    google: any;
+  }
+}
+
+export function GoogleTranslate() {
+  useEffect(() => {
+    // Define a função global que o script do Google vai chamar
+    (window as any).googleTranslateElementInit = () => {
+      if ((window as any).google && (window as any).google.translate) {
+        new (window as any).google.translate.TranslateElement(
+          {
+            pageLanguage: 'pt',
+            layout: (window as any).google.translate.TranslateElement.InlineLayout.SIMPLE,
+          },
+          'google_translate_element'
+        );
+      }
+    };
+  }, []);
+
+  return <div id="google_translate_element" style={{ display: 'inline-block', marginRight: '15px' }} />;
+}
